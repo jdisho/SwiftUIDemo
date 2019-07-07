5573261c56cf24342c98af179e32e26984520bb4
Joan Disho <dishojoan@gmail.com>
2019-07-07 13:25:23 +0200

    create DataService

---
+++ SwiftUIDemo/SwiftUIDemo/Services/DataService.swift
@@ -0,0 +1,31 @@
+//
+//  DataService.swift
+//  PaprSwiftUI
+//
+//  Created by Joan Disho on 06.07.19.
+//  Copyright © 2019 disho. All rights reserved.
+//
+
+import Foundation
+
+final class DataService {
+    static func load<T: Decodable>(_ filename: String, as type: T.Type = T.self) -> T {
+        let data: Data
+
+        guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+        }
+
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+        }
+
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+        }
+    }
+}
